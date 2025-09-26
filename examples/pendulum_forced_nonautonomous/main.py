f67ff8eff0d0bb0f373b6969cae12144eee59bdf
Tiago Martins <tiago.sm99@gmail.com>
2025-09-26 18:41:30 +0200

    Add forced pendulum example with Duffing system
    Introduces a new example for a forced, nonautonomous pendulum using a Duffing oscillator. Adds the `DynamicalSystem` and `DuffingForced` classes, along with a main script that sets up and solves the system using the Harmonic Balance Method. This provides a template for analyzing forced nonlinear oscillators.

---
+++ examples/pendulum_forced_nonautonomous/main.py
@@ -0,0 +1,45 @@
+# %%
+
+from dynamical_system import *
+
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+
+from pyhbm import *
+
+duffing = DuffingForced(c=0.07, k=1.0, P=0.1)  # Create an instance of Duffing
+
+duffing_solver = HarmonicBalanceMethod(
+    first_order_ode = duffing, 
+    harmonics = [1,3,5,7,9], 
+)
+
+# Define the initial guess after defining the harmonics of the HarmonicBalanceMethod
+initial_omega = 0.0
+first_harmonic = np.array([[1],[1j*initial_omega]])
+static_amplitude = duffing.P/duffing.k
+initial_guess = FourierOmegaPoint.new_from_first_harmonic(first_harmonic * static_amplitude, omega=initial_omega)
+initial_reference_direction = FourierOmegaPoint.new_from_first_harmonic(first_harmonic, omega=1)
+
+solution_set = duffing_solver.solve_and_continue(
+    initial_guess = initial_guess, 
+    initial_reference_direction = initial_reference_direction, 
+    maximum_number_of_solutions = 1000000, 
+    angular_frequency_range = [0.0, 10], 
+    solver_kwargs = {
+        "maximum_iterations": 200, 
+        "absolute_tolerance": duffing.P * 1e-6
+    }, 
+    step_length_adaptation_kwargs = {
+        "base": 2, 
+        "initial_step_length": 0.1, 
+        "maximum_step_length": 2.0, 
+        "minimum_step_length": 5e-6, 
+        "goal_number_of_iterations": 3
+    }
+)
+
+solution_set.plot_FRF(degrees_of_freedom=0)
+# %%
